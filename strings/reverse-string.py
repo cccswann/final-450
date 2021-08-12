f65b5fc45313e0be338121eb1252502418029b1c
Ciara Swann <cccswann@gmail.com>
2021-08-12 14:13:27 -0700

    Create strings directory.

---
+++ strings/reverse-string.py
@@ -0,0 +1,28 @@
+# Reverse a String Problem
+# https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+# Implemented in practice first. Reverse a string.
+
+# Iterative Approach
+
+def reverseWord(s):
+    # Change string to list - strings are immutable in python
+    list1 = list(s)
+    # Create 2 pointers - start and end
+    start = 0 
+    end = len(list1) - 1
+    # In a loop swap the start with the end pointer 
+    while start < end:
+        list1[start], list1[end] = list1[end], list1[start]
+        start += 1
+        end -= 1
+    # Revert list back to string 
+    str1 = ''.join(list1) 
+    return str1
+
+# Another approach using Python list slicing
+
+def reverseString(s):
+    list1 = list(s)
+    new = list1[::-1]
+    str1 = ''.join(new)
+    return str1
